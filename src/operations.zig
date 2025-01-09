a444438b885387664a6a8de9d2e94352e87dabfb
ItsMeSamey <sameychain5041@gmail.com>
2025-01-09 13:34:33 +0530

    rectified help message

--- src/operations.zig
+++ src/operations.zig
@@ -26,7 +26,7 @@ pub fn printUsageAndExit() noreturn {
   const my_name = myName();
 
   nosuspend stdout.print(
-    \\Usage: {s} [options] args ...
+    \\Usage: {s} [options] [mode]
     \\
     \\Options:
     \\  help             Display this help message and exit.
@@ -39,24 +39,27 @@ pub fn printUsageAndExit() noreturn {
     \\      auto         Automatically detact and run in daemon / systemd mode
     \\      daemon       Start in daemon mode (not associated with systemd or any other system)
     \\      systemd      Start in systemd mode, sys
-    \\  stop             Stop this program / daemon running in background
+    // \\  stop             Stop this program / daemon running in background
     \\  install          Install this program in auto mode
     \\  install [mode]   Install this program
     \\    mode:
     \\      auto         Automatically detect and install to /bin and if possible systemd service
     \\      bin          Install to /usr/bin
     \\      systemd      Install to /usr/bin + as systemd service as well
-    \\  uninstall        Stop and Uninstall this program, also remove any autostart entries
-    \\  uninstall [mode] Stop and Uninstall this program, also remove any autostart entries
+    \\  uninstall        Uninstall this program
+    \\  uninstall [mode] Uninstall this program, also remove any autostart entries
     \\    mode:
-    \\      auto         Automatically remove from /bin and if possible systemd service
-    \\      bin          Remove from /usr/bin
+    \\      bin          Remove from /usr/bin + remove all services (does not stop running services)
     \\      systemd      Remove systemd service
+    \\  enable           Enable in auto mode
     \\  enable [mode]    Enable this program, program must be installed first
     \\    mode:
+    \\      auto         Automatically detect and enable. If not installed / already enable, no-op
     \\      systemd      Enable autostart using systemd
+    \\  disable         Disable in auto mode
     \\  disable [mode]   Disable this program, no-op if not installed / enabled
     \\    mode:
+    \\      auto         Automatically detect and disable. If not installed / already disabled, no-op
     \\      systemd      Disable autostart using systemd
     \\  status           Show status of running services
     \\
